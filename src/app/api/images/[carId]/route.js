503855e50b812123ec79a8335245157e6ff09d32
Alfonso Reyes <alfonsoreyes656@gmail.com>
2026-03-08 19:40:57 +0100

    cambios realizados

--- src/app/api/images/[carId]/route.js
+++ src/app/api/images/[carId]/route.js
@@ -2,23 +2,22 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET(request, { params }) {
-    // Obtenemos el carId de los parámetros de la URL
-    const { carId } = params;
-
     try {
+        const { carId } = await params; 
+
         const images = await prisma.carImage.findMany({
             where: { 
                 carId: parseInt(carId) 
             }
         });
 
-        if (images.length === 0) {
-            return NextResponse.json({ message: "No se encontraron imágenes para este coche" }, { status: 404 });
+        if (!images || images.length === 0) {
+            return NextResponse.json({ message: "No se encontraron imágenes" }, { status: 404 });
         }
 
         return NextResponse.json(images, { status: 200 });
     } catch (error) {
         console.error("Error al obtener imágenes:", error);
-        return NextResponse.json({ error: "Error interno al buscar imágenes" }, { status: 500 });
+        return NextResponse.json({ error: "Error interno" }, { status: 500 });
     }
 }
